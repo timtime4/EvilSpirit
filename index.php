3ca917ab73ae6fde5804cc8f3c9f7cce53c35a7d
Timothy Joseph Pusateri <tpusater@dsg1.virtual.crc.nd.edu>
2015-11-02 20:48:16 -0500

    Beginning Login

---
+++ index.php
@@ -0,0 +1,24 @@
+
+
+<html>
+  <head>
+    <title>Welcome!</title>
+  </head>
+  <body>
+    <h1>Welcome to the Campus Housing Work Order Management System</h1>
+    <h2>Please log in</h2>
+    <form id="login" action="#">
+      <div>
+        <label for="username">Username:</label>
+        <input id="username" type="text">
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input id="username" type="text">
+      </div>
+      <div>
+        <input type="submit" value="Log In">
+      </div>
+    </form>
+  </body>
+</html
---
+++ validate_login.php
@@ -0,0 +1,7 @@
+<?php
+// Connect to database
+$link = mysqli_connect('localhost', 'tpusater', 'evilspirit')
+  or die('Could not connect: ' . mysql_error());
+echo 'Connected successfully!';
+
+?>
