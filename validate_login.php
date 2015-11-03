66169300ddb895e445d5293e857a849311b45b5d
Tim Pusateri <tpusateri@moresteam.com>
2015-11-02 23:49:20 -0500

    added mad php

--- validate_login.php
+++ validate_login.php
@@ -13,12 +13,32 @@ session_start();
 			$username = $_POST["username"];
 			$password = $_POST["password"];
 		?>
-		
+
 		<?php
 		  // Connect to database
 		  $link = mysqli_connect('localhost', 'tpusater', 'evilspirit')
 		    or die('Could not connect: ' . mysql_error());
 		  echo 'Connected successfully!';
+
+		  mysqli_select_db($link, 'tpusater') or die('Could not select database');
+			
+			// Performing SQL query
+			$query = 'SELECT username, password FROM Users WHERE username = $username';
+			$result = mysqli_query($link, $query) or die('Query failed: ' . mysql_error());
+			// Printing results in HTML
+			echo "<table>\n";
+			while ($tuple = mysqli_fetch_array($result, MYSQL_ASSOC)) {
+ 			echo "\t<tr>\n";
+ 			foreach ($tuple as $col_value) {
+			 	echo "\t\t<td>$col_value</td>\n";
+			 }
+			 echo "\t</tr>\n";
+			}
+			echo "</table>\n";
+			// Free resultset
+			mysqli_free_result($result);
+			// Closing connection
+			mysqli_close($link);
 		?>
 	</body>
 
