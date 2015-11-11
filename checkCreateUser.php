d638dc5ecd8a769378a8ac6a0c995ef4a205daa6
Timothy Joseph Pusateri <tpusater@dsg1.virtual.crc.nd.edu>
2015-11-11 18:10:10 -0500

    Correct insert string

--- checkCreateUser.php
+++ checkCreateUser.php
@@ -31,38 +31,40 @@
 			mysqli_select_db($link, 'tpusater') or die('Could not select database');
 
 			$fields[] = "username";
-			$values[] = $_POST["username"];
+			$values[] = "'" . $_POST["username"] . "'";
 
 			// Deal with password
 			$fields[] = "password";
-			$values[] = password_hash($_POST["password"], PASSWORD_DEFAULT);
+			$values[] = "'" . password_hash($_POST["password"], PASSWORD_DEFAULT) . "'";
 
 			if($_POST["firstName"] != ""){
 				$fields[] = "firstName";
-				$values[] = $_POST["firstName"];
+				$values[] = "'" . $_POST["firstName"] . "'";
 			}
 
 			if($_POST["lastName"] != ""){
 				$fields[] = "lastName";
-				$values[] = $_POST["lastName"];
+				$values[] = "'" . $_POST["lastName"] . "'";
 			}
 
 			if($_POST["email"] != ""){
 				$fields[] = "email";
-				$values[] = $_POST["email"];
+				$values[] = "'" . $_POST["email"] . "'";
 			}
 
 			if($_POST["phoneNum"] != ""){
 				$fields[] = "phoneNum";
-				$values[] = $_POST["phoneNum"];
+				$values[] = "'" . $_POST["phoneNum"] . "'";
 			}
 
 			$newUserSubmit = "INSERT INTO Users(" .  
 				implode(",", $fields) . ") VALUES (" .
 				implode(",", $values) . ")";
 
+      echo $newUserSubmit;
+
       $newUserSumbitResult = mysqli_query($link, $newUserSubmit) 
-				or die('Insert Query failed: ' . mysql_error());
+				or die('Insert Query failed: ' . mysql_error($link));      
 
     }
   }
